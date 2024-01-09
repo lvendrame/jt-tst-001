fc56ba96fff06534c0950a1a17e949216fce7d51
Jaimie (Jitera) <developers@jitera.com>
2024-01-09 14:15:22 +0000

    feat[service]: implement Validate shop information

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,19 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  validateShopInformation(shop_name: string, contract_status: string): boolean | string {
+    if (typeof shop_name !== 'string' || shop_name.length > 50) {
+      return '50 文字以内で入力してください';
+    }
+
+    const contractStatusBoolean = contract_status.toLowerCase() === 'yes' ? true : contract_status.toLowerCase() === 'no' ? false : null;
+    if (contractStatusBoolean === null) {
+      return 'contract_status must be a boolean value (Yes/No)';
+    }
+
+    return true;
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
